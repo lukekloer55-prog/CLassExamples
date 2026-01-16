e7d256d0c3cfb9963fd613f9090589cc01b34e6f
Luke K <Luke K@DESKTOP-1J2J4KT>
2026-01-16 08:53:51 -0700

    End of 1/16/26

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose A Number:"); int Firstnumber = Console.Read();
+            Console.WriteLine("Choose Another Number:"); int Secondnumber = Console.Read();
+            Console.WriteLine("Choose one of the following operations:");
+            Console.WriteLine("1. Sum");
+            Console.WriteLine("2. Product");
+            Console.WriteLine("3. Diffrence");
+            Console.WriteLine("4. Qoutient");
+            int ChosenOperation = Console.Read();
+            
+        }
+    }
+}
